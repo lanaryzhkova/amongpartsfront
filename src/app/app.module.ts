3e4b416ea252854eddc556220585f1980fc3ea52
Lana Ryzhkova <svetlana.ryzhkova2002@gmail.com>
2022-04-22 14:04:55 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { ActionCardComponent } from './components/action-card/action-card.component';
+import { HomePartCardComponent } from './components/home-part-card/home-part-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ActionCardComponent,
+    HomePartCardComponent,
+    HomeComponent,
+    ArticleCardComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
